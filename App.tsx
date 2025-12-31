35e8959465a2b1d39330f7fd65ee258098599c54
AkraMtello12 <98246202+AkraMtello12@users.noreply.github.com>
2025-12-31 16:22:03 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import LoginPage from './pages/Login';
 import { Button } from './components/UIComponents';
 
 // Placeholder Logo URL
-const LOGO_URL = "https://cdn-icons-png.flaticon.com/512/2881/2881031.png";
+const LOGO_URL = "https://drive.google.com/file/d/13oRBNAsLnLzwUasK43KLhcs2u82EiM8T/view?usp=sharing";
 
 const Navbar = () => {
   const location = useLocation();
